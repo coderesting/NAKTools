add3ddb8ad5c04479489432c5a01ccfa4614109e
Yannick Brandt <yannick-brandt@outlook.de>
2021-05-26 16:48:44 +0200

    Updated NAKExtension description

--- src/toolDescriptions.ts
+++ src/toolDescriptions.ts
@@ -10,7 +10,7 @@ const toolDescriptions = [
 	{
 		name: "NAK Extension",
 		description:
-			"Makes Moodle and Cis better. Current features are auto login and showing all courses in the moodle sidebar.",
+			"Enhance Moodle, Cis and Owa with this Chrome extension. Features include auto login, Moodle course suggestions and more.",
 		appUrl: "/nakextension/",
 		repoUrl: "https://github.com/coderesting/NAKExtension",
 		image: "assets/NAKExtension-256x256.png",
@@ -18,7 +18,7 @@ const toolDescriptions = [
 	{
 		name: "Schedule Cleaner",
 		description:
-			"Filter unnecessary junk from your NAK Schedule. You can use a URL or convert an ICS File to get the schedule",
+			"Filter unnecessary junk from your NAK Schedule. You can use a URL or convert an ICS File to get the schedule.",
 		appUrl: "https://schedule-cleaner.herokuapp.com/",
 		repoUrl: "https://github.com/jeff-saupe/ScheduleCleaner",
 		image: "assets/ScheduleCleaner-256x256.png",
